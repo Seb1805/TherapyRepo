5a35a781076a7a7083148ec7a6e35d5b0913aa55
Chigeru <frankjensen156@gmail.com>
2025-03-10 13:51:06 +0100

    Patient start layout

--- client/components/journal-note.jsx
+++ client/components/journal-note.jsx
@@ -6,7 +6,7 @@ export default function JournalNote({ journalEntry }) {
     <div>
       <div className="text-gray-500 text-sm">
         <label>
-          {format(journalEntry.date, "d MMM. yyyy", { locale: da })}
+          {format(journalEntry?.date, "d MMM. yyyy", { locale: da })}
         </label>
         <div className="flex">
           <label className="">user role</label>
@@ -16,21 +16,21 @@ export default function JournalNote({ journalEntry }) {
       </div>
       <div>
         <h2>jounral treatment</h2>
-        {journalEntry.exerciseRecommendations.length > 0 && (
+        {journalEntry?.exerciseRecommendations.length > 0 && (
           <>
             <p>exerciseRecommendations name</p>
             <ul>
-              {journalEntry.exerciseRecommendations.map((execise) => {
+              {journalEntry?.exerciseRecommendations.map((execise) => {
                 <li>{execise.execiseInfo.name}</li>;
               })}
             </ul>
           </>
         )}
-        <p>{journalEntry.notes}</p>
-        {journalEntry.treatmentPlan.length > 0 && (
+        <p>{journalEntry?.notes}</p>
+        {journalEntry?.treatmentPlan.length > 0 && (
           <>
             <h3>Behandlingsplan</h3>
-            <p>{journalEntry.treatmentPlan}</p>
+            <p>{journalEntry?.treatmentPlan}</p>
           </>
         )}
       </div>
